1ab966f0900ec028836a5ab27ac9aa6792331888
Nikolai <32290850+nmatanski@users.noreply.github.com>
2018-02-17 20:02:24 +0200

    Update AppDbContext.cs

--- DAL.Database/AppDbContext.cs
+++ DAL.Database/AppDbContext.cs
@@ -13,14 +13,6 @@ namespace DAL.Database
             //connection = DBConnections.GetAzureConnection();
         }
 
-        //public DbSet<ITGigs.UserService.Domain.Models.User> Users { get; set; }
-
-        //public DbSet<ITGigs.VenueService.Domain.Models.Venue> Venues { get; set; }
-
-        //public DbSet<ITGigs.ITGigService.Domain.Models.ITGig> ITGigs { get; set; }
-
-        //public DbSet<ITGigs.LogService.Domain.Models.CustomException> CustomExceptions { get; set; }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(connection);
